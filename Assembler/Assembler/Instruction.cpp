d4df8e82124e48abac014ffd8c95832c9aeff251
Anuj Bastola <abastol1@ramapo.edu>
2018-05-29 14:48:33 -0400

    Add files via upload

---
+++ Assembler/Assembler/Instruction.cpp
@@ -0,0 +1,8 @@
+#include "Instruction.h"
+using namespace std;
+
+
+InstructionType ParseInstruction(string &a_buff);
+
+// Compute the location of the next instruction.
+int LocationNextInstruction(int a_loc);
